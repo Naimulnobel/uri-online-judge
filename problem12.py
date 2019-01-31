d318496a44e4f40db128f8351499302eb206c261
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-31 17:39:49 +0600

    Add files via upload

---
+++ problem12.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 17:06:45 2019
+
+@author: Student Mct
+"""
+
+a=float(input())
+b=float(input())
+c=float(input())
+TRIANGULO=(1/2)*a*c
+CIRCULO=3.14159*c*c
+trapezium=(a+c/2)*c
+QUADRADO=b*b
+RETANGULO=a*b
+print('TRIANGULO:',TRIANGULO)
+print('CIRCULO:',CIRCULO)
+print('trapezium:',trapezium)
+print('QUADRADO:',QUADRADO)
+print('RETANGULO:',RETANGULO)
