bc7e9d0c6a816961a9659740de5c09e62a261d9c
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-31 15:34:48 +0600

    Add files via upload

---
+++ problem7.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 15:15:01 2019
+
+@author: Student Mct
+"""
+a=input()
+b=input()
+c=input()
+d=input()
+value1=int(a)*int(b)
+value2=int(c)*int(d)
+DIFERENCA=value1-value2
+print(' DIFERENCA  :', DIFERENCA)
---
+++ problem8.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 15:23:55 2019
+
+@author: Student Mct
+"""
+
+a=input('Employee Number ')
+b=input()
+c=input()
+amount=int(b)*float(c)
+print('NUMBER =',a)
+print('SALARY = U$',amount)
