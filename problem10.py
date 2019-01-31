6f10dfb9717bbb5cc114fd2a832876181719ecbd
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-31 16:57:02 +0600

    Add files via upload

---
+++ problem10.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 16:01:59 2019
+
+@author: Student Mct
+"""
+Id=int(input('id '))
+q=float(input('units '))
+prize=float(input('prize '))
+Id2=int(input('id '))
+q2=float(input('units '))
+prize2=float(input('prize '))
+
+total=(q*prize)
+total1=(q2*prize2)
+Sum=total+total1
+print('VALOR A PAGAR: R$',Sum)
+
+
---
+++ problem9.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 15:46:44 2019
+
+@author: Student Mct
+"""
+a=input()
+b=float(input())
+c=float(input())
+parcent=(15*c)/100
+total=parcent+b
+print('TOTAL = R$',total)
