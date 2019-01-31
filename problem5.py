a759e6db4e41424bd04557d0e7a2becd437eaa8c
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-31 14:42:19 +0600

    Add files via upload

---
+++ problem5.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 13:42:23 2019
+
+@author: Student Mct
+"""
+
+a=input()
+b=input()
+c=float(a)*float(3.5)
+d=float(b)*float(7.5)
+sum1=float(c)+float(d)
+sum2=float(3.5)+float(7.5)
+MEDIA=sum1/sum2
+print('MEDIA =',MEDIA)
---
+++ problem6.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 14:34:08 2019
+
+@author: Student Mct
+"""
+
+a=input()
+b=input()
+c=input()
+d=float(a)*float(2)
+e=float(b)*float(3)
+f=float(c)*float(5)
+sum1=float(d)+float(e)+float(f)
+sum2=float(2)+float(3)+float(5)
+MEDIA=sum1/sum2
+print('MEDIA =',MEDIA)
