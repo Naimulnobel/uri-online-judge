dfa548976b5d333cd0a2e8921381d271d23458b5
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-31 18:10:43 +0600

    Add files via upload

---
+++ problem14.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 18:03:43 2019
+
+@author: Student Mct
+"""
+a=int(input('Distance '))
+b=float(input('Spent Fuel '))
+spend=a/b
+print(str(spend),'Km/l')
