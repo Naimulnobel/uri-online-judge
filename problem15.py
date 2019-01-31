93e92d65ee3da0fc3029745dcf0090ab4ba02b17
Naimul Islam Nobel <34490559+Naimulnobel@users.noreply.github.com>
2019-01-31 18:29:30 +0600

    Add files via upload

---
+++ problem15.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 31 18:17:13 2019
+
+@author: Student Mct
+"""
+
+import math
+x1=float(input())
+x2=float(input())
+y1=float(input())
+y2=float(input())
+ex=x2-x1
+ex1=y2-y1
+result=math.sqrt(ex*ex-ex1*ex1)
+print(result)
